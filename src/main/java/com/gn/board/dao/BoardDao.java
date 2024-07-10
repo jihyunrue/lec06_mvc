f3f32e5f5cc2b1740c4864f316745fd9995235ca
jhrue <jhrue7@gmail.com>
2024-07-10 15:50:44 +0900

    [류지현]게시글목록조회

--- src/main/java/com/gn/board/dao/BoardDao.java
+++ src/main/java/com/gn/board/dao/BoardDao.java
@@ -1,8 +1,13 @@
 package com.gn.board.dao;
 
+import static com.gn.common.sql.JDBCTemplate.close;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import static com.gn.common.sql.JDBCTemplate.close;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gn.board.vo.Board;
 
 public class BoardDao {
@@ -28,6 +33,40 @@ public class BoardDao {
 			close(pstmt);
 		}
 		return result;
+	}
+	
+	public List<Board> selectBoardList(Board option, Connection conn){
+		List<Board> list = new ArrayList<Board>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// 검색 조건
+			// x : SELECT * FROM board
+			// O : SELECT * FROM board WHERE board_title LIKE CONCAT('%',board_title,'%')
+			String sql = "SELECT * FROM board";
+			if(option.getBoard_title() != null) {
+				sql+= " WHERE board_title LIKE CONCAT('%','"+option.getBoard_title()+"','%')";
+			}
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Board resultVo = new Board(rs.getInt("board_no"),
+						rs.getString("board_title"),
+						rs.getString("board_content"),
+						rs.getInt("board_writer"),
+						rs.getTimestamp("reg_date").toLocalDateTime(),
+						rs.getTimestamp("mod_date").toLocalDateTime(),
+						rs.getString("ori_thumbnail"),
+						rs.getString("new_thumbnail"));
+				list.add(resultVo);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		return list;
 	}
 
 }
